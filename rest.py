ef9043636deb4f22e783bc28fb9f01e6d4a41735
shdown <shdownnine@gmail.com>
2018-05-19 12:21:43 +0300

    rest.py: introduce sessions and auth

--- rest.py
+++ rest.py
@@ -1,7 +1,19 @@
-from urllib.parse import urlencode
 import logging
 import requests
 import auth_utils
+
+
+class _PoloniexAuth(requests.auth.AuthBase):
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def __call__(self, r):
+        r.headers.update({
+            'Sign': auth_utils.generate_signature(self.api_secret, r.body or ''),
+            'Key': self.api_key,
+        })
+        return r
 
 
 class RestError(Exception):
@@ -12,6 +24,8 @@ class RestSession:
     def __init__(self, api_key, api_secret):
         self.api_key = api_key
         self.api_secret = api_secret
+        self._session = requests.Session()
+        self._auth = _PoloniexAuth(api_key, api_secret)
         self.logger = logging.getLogger('angou_poloniex')
 
     def request(self, command, params=None):
@@ -23,13 +37,11 @@ class RestSession:
             'nonce': auth_utils.generate_nonce(),
         })
 
-        post_data = urlencode(params)
-        req = requests.get('https://poloniex.com/tradingApi', data=post_data, headers={
-            'Sign': auth_utils.generate_signature(self.api_secret, post_data),
-            'Key': self.api_key,
-        })
-        req.raise_for_status()
-        resp = req.json()
+        req = requests.Request('GET', 'https://poloniex.com/tradingApi',
+                               data=params, auth=self._auth)
+        r = self._session.send(self._session.prepare_request(req))
+        r.raise_for_status()
+        resp = r.json()
 
         if 'error' in resp:
             raise RestError(resp['error'])
